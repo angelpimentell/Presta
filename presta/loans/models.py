0c7dd61f6f4b80dca99da5be14585b169c996f00
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-12 16:00:22 -0400

    [ADD] Loan model

--- presta/loans/models.py
+++ presta/loans/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Loan(models.Model):
+    client = models.ManyToManyField("users.User")
+    payment_time = models.ManyToManyField("loans.Time")
+    total_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    due_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    interests = models.DecimalField(max_digits=3, decimal_places=2)
+    fee_type = models.PositiveSmallIntegerField()
+    start = models.DateField()
+    finish = models.DateField(null=True)
+
