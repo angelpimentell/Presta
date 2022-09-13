215d5ab070019c44af4f07f54cdd487c591a8902
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-13 09:08:11 -0400

    [ADD] Setting model to settings module

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Setting(models.Model):
+    company_name = models.CharField(max_length=20)
+    company_avatar = models.ImageField(upload_to="avatar/")
+    default_currency = models.PositiveSmallIntegerField()
