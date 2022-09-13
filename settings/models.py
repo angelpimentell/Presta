4138c6f871da3a60984e8554fd1875e32df58c95
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-13 09:09:27 -0400

    [ADD] __str__ function to Setting model of Settings module

--- settings/models.py
+++ settings/models.py
@@ -5,3 +5,6 @@ class Setting(models.Model):
     company_name = models.CharField(max_length=20)
     company_avatar = models.ImageField(upload_to="avatar/")
     default_currency = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.setting_text
