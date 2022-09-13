86a16d7132e49cb71633f6650907faead1719a8c
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-13 08:42:44 -0400

    [ADD] Models to loans module
    Client, PaymentTime and Payment models created

--- presta/loans/models.py
+++ presta/loans/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
+class Client(models.Model):
+    email = models.CharField(max_length=60)
+    direction = models.CharField(max_length=80)
+    telephone_number = models.PositiveBigIntegerField()
+    telephone_ext = models.PositiveSmallIntegerField()
+    avatar = models.ImageField(upload_to="avatars/")
+    description = models.CharField(max_length=100)
+
+
+class PaymentTime(models.Model):
+    name = models.CharField(max_length=20)
+    measure_type = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+
+
 class Loan(models.Model):
-    client = models.ManyToManyField("users.User")
-    payment_time = models.ManyToManyField("loans.Time")
+    client = models.ManyToManyField("loans.Client", db_table='loans_clients')
+    payment_time = models.OneToOneField("loans.PaymentTime", on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=9, decimal_places=2)
     due_amount = models.DecimalField(max_digits=9, decimal_places=2)
     interests = models.DecimalField(max_digits=3, decimal_places=2)
@@ -11,3 +26,12 @@ class Loan(models.Model):
     start = models.DateField()
     finish = models.DateField(null=True)
 
+
+class Payment(models.Model):
+    loan = models.ForeignKey("loans.Loan", on_delete=models.CASCADE)
+    payment_time = models.ForeignKey("loans.PaymentTime", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    date = models.DateField()
+    delay = models.PositiveSmallIntegerField()
+    interests = models.DecimalField(max_digits=3, decimal_places=2)
+    interests = models.PositiveSmallIntegerField()
