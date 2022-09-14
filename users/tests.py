5e3c70bb671ccda8f7309fadd6a1b8a799b1a1d2
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-14 15:55:50 -0400

    [ADD] Test class of example

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(name="lion",
+                            email="roar",
+                            password="roar",
+                            avatar="image.jpg",
+                            description="roar")
+
+        def test_animals_can_speak(self):
+            lion = User.objects.get(name="lion")
+            self.assertEqual(lion, 'The lion says "roar"')
