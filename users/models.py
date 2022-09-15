7398f7d75b61d397d3e434d85a0147fe841d0522
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-15 15:22:56 -0400

    [ADD] Field name to User model

--- users/models.py
+++ users/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class User(models.Model):
     rol = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=50)
     email = models.CharField(max_length=60)
     password = models.CharField(max_length=256)
     avatar = models.ImageField(upload_to="avatars/")
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user_text
+        return self.name
