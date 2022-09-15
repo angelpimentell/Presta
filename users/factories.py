62dbfacd76ed705308118197ad4440a62ab65fd2
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-15 15:22:37 -0400

    [ADD] User factory class

--- users/factories.py
+++ users/factories.py
@@ -1,1 +1,17 @@
-import factory
+from factory.django import DjangoModelFactory
+from .models import User
+from faker import Faker
+
+fake = Faker()
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    rol = fake.random_int(min=0, max=3)
+    name = fake.name()
+    email = fake.email()
+    password = fake.password(length=12)
+    avatar = fake.file_path(depth=5, category='image')
+    description = fake.text(max_nb_chars=100)
