bf4f728c31898ae2bfa520f9fa9e5f9dde9ac034
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-17 07:48:08 -0400

    [ADD] Loan database test

--- loans/tests/tests.py
+++ loans/tests/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from ..factories import LoanFactory
 
 
-class LoanTestCase(TestCase):
-    pass
+class UserTestCase(TestCase):
+    def test_new_loan(self):
+        loan_example = LoanFactory()
+        self.assertIsNotNone(loan_example)
