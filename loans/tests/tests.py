a0d60d7773d35fc3bab24e4dec9ef3a38b63574d
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-19 21:09:37 -0400

    [CHANGE] Loan tests

--- loans/tests/tests.py
+++ loans/tests/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 
 from ..factories.LoanFactory import LoanFactory
+from ..factories.PaymentTimeFactory import PaymentTimeFactory
+from ..factories.ClientFactory import ClientFactory
 
 
 class LoanTestCase(TestCase):
 
     def test_new_loan(self):
-        loan_example = LoanFactory()
+        client = ClientFactory()
+        loan_example = LoanFactory.create(clients=(client,))
         self.assertIsNotNone(loan_example)
