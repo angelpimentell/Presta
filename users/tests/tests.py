b45041fe7d6456f37287321b0038dafb10395b30
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-09-30 14:10:58 -0400

    [CHANGE] test.py name in users\tests

--- users/tests/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from ..factories.UserFactory import UserFactory
-
-
-class UserTestCase(TestCase):
-    def test_new_user(self):
-        user_example = UserFactory()
-        self.assertIsNotNone(user_example)
