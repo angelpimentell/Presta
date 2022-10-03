091191426b3115d22f4c7e53bb904bff9d571179
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-03 10:08:50 -0400

    [FIX] Max functional digits in Loan and Payment fields

--- loans/models/Loan.py
+++ loans/models/Loan.py
@@ -4,9 +4,9 @@ from django.db import models
 class Loan(models.Model):
     client = models.ManyToManyField("loans.Client", db_table='loans_clients')
     payment_time = models.OneToOneField("loans.PaymentTime", on_delete=models.CASCADE)
-    total_amount = models.DecimalField(max_digits=9, decimal_places=2)
-    due_amount = models.DecimalField(max_digits=9, decimal_places=2)
-    interests = models.DecimalField(max_digits=3, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    due_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    interests = models.DecimalField(max_digits=5, decimal_places=2)
     fee_type = models.PositiveSmallIntegerField()
     start = models.DateField()
     finish = models.DateField(null=True)
