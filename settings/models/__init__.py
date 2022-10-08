94e86bcbbd468363d348e7857513418a3d705706
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-08 13:49:37 -0400

    [ADD] Dynamic import

--- settings/models/__init__.py
+++ settings/models/__init__.py
@@ -1,1 +1,13 @@
-from . import Setting
+from . import *
+import types
+
+
+def imports():
+    for name, val in globals().items():
+        if isinstance(val, types.ModuleType):
+            if '.' not in val.__name__ and 'types' not in val.__name__:
+                name = val.__name__
+                __import__('settings.models.' + name.capitalize())
+
+
+imports()
