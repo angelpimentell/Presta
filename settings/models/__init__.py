3f16fc1a2bb88d181e39015eaa4934e1e4b6b32b
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-09 12:02:19 -0400

    [CHANGE] Model importation logic

--- settings/models/__init__.py
+++ settings/models/__init__.py
@@ -1,7 +1,3 @@
-from os.path import dirname, basename, isfile, join
-import glob
+from common.Facades.Package import Package
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
-for one in __all__:
-    __import__('settings.models.' + one)
+Package(__file__).import_all_modules()
