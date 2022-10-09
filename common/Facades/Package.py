068acbe8e997074c80c44e1322435fd07608141c
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-09 11:33:39 -0400

    [CHANGE] Package facade logic

--- common/Facades/Package.py
+++ common/Facades/Package.py
@@ -6,9 +6,15 @@ class Package:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def import_all_modules_from_models(self):
-        package_name = self.file_path.split('\\')[-3]
-        modules_path = glob.glob(join(dirname(self.file_path), "*.py"))
-        modules_name = [basename(f)[:-3] for f in modules_path if isfile(f) and not f.endswith('__init__.py')]
-        for module in modules_name:
-            __import__(package_name + '.models.' + module)
+    def import_all_modules(self):
+        app_name = self.file_path.split('\\')[-3]
+        package_name = self.file_path.split('\\')[-2]
+        module_names = self.get_module_names()
+
+        for module_name in module_names:
+            __import__(app_name + '.' + package_name + '.' + module_name)
+
+    def get_module_names(self):
+        module_paths = glob.glob(join(dirname(self.file_path), "*.py"))
+        module_names = [basename(f)[:-3] for f in module_paths if isfile(f) and not f.endswith('__init__.py')]
+        return module_names
