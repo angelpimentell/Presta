1f6028c265d065d04daf00c051608db039e31ef5
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-16 06:46:17 -0400

    [CHANGE] Way to run commands

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+
+# Make migrations
+for folder in $(ls -d */); do
+  module_name="${folder//\//}";
+
+  if [ "$module_name" != 'venv' ] && [ "$module_name" != 'common' ] && [ "$module_name" != 'presta' ]
+  then
+    /usr/local/bin/python manage.py makemigrations "$module_name"
+  fi
+done
+
+# Migrate
+/usr/local/bin/python manage.py migrate
+
+# Create superuser
+/usr/local/bin/python manage.py createsuperuser --noinput
+
+# Run server
+/usr/local/bin/python manage.py runserver 0.0.0.0:8000
