e2d94243f6edae930e5754454f8dc54271fc436b
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-25 05:58:14 -0400

    [ADD] Method in database repository

--- common/repositories/database_repository.py
+++ common/repositories/database_repository.py
@@ -1,5 +1,38 @@
-from common.repositories.interface_repository import InterfaceRespository
+from common.repositories.interface_repository import InterfaceRepository
 
 
-class DatabaseRepository(InterfaceRespository):
-    pass
+class DatabaseRepository(InterfaceRepository):
+    def __init__(self, model):
+        self.model = model
+
+    def get_by_id(self, id: int, columns: tuple = ()):
+        return self.model.objects.filter(id=id).values(*columns)
+
+    def get_by(self, columns: tuple = (), **kwargs):
+        return self.model.objects.filter(**kwargs).values(*columns)
+
+    def where_in(self):
+        pass
+
+    def list(self):
+        pass
+
+    def list_by(self):
+        pass
+
+    def list_where_like(self):
+        pass
+
+    def associate(self):
+        pass
+
+    def create(self, **kwargs):
+        return self.model.objects.create(**kwargs)
+
+    def update(self, **kwargs):
+        # TODO: Find way to raise ObjectDoesNotExist when filter count is 0
+        self.model.objects.filter(pk=kwargs['id']).update(**kwargs)
+        return kwargs['id']
+
+    def delete(self, id: int):
+        return self.model.objects.filter(id=id).delete()
