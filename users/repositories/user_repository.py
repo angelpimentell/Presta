6fdcc899918cf952dff8bb17bc6487de69795c03
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-25 15:32:28 -0400

    [CHANGE] Class name

--- users/repositories/user_repository.py
+++ users/repositories/user_repository.py
@@ -2,6 +2,6 @@ from users.models.User import User
 from common.repositories.database_repository import DatabaseRepository
 
 
-class UserRepository(DatabaseRepository):
+class UserDatabaseRepository(DatabaseRepository):
     def __init__(self):
         super().__init__(User)
