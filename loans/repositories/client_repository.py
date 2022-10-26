40c24c2fe1ad7cf14b3f4b34ef9d60ecce999221
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-26 07:03:53 -0400

    [ADD] Important comment

--- loans/repositories/client_repository.py
+++ loans/repositories/client_repository.py
@@ -2,6 +2,7 @@ from loans.models.client import Client
 from common.repositories.database_repository import DatabaseRepository
 
 
+# TODO: Specify that onlye you can import the ClientDatabaseRepository
 class ClientDatabaseRepository(DatabaseRepository):
     def __init__(self):
         super().__init__(Client)
