56b4d74659562d4b16217bfb41838e23d773178f
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-26 07:44:19 -0400

    [ADD] Line

--- users/views/user_view.py
+++ users/views/user_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+
 class UserViewSet(viewsets.ViewSet):
     """
     Example empty viewset demonstrating the standard
