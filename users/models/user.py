e8f3768cd4f67764291101c91d0164be158fdd0f
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-27 09:05:18 -0400

    [CHANGE] Change arguments in User model

--- users/models/user.py
+++ users/models/user.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=256)
-    avatar = models.ImageField(upload_to="avatars/")
+    avatar = models.ImageField(upload_to="avatars/", null=True)
     description = models.CharField(max_length=100)
 
     def __str__(self):
