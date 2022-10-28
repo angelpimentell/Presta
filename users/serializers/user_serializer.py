824f7bff84c70e8a1ba7e3c739cc2dc1b1978945
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-28 08:42:10 -0400

    [CHANGE] Logic in UserSerializer

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
+from users.models.user import User
 
 
-class UserSerializer(serializers.Serializer):
-    rol = serializers.IntegerField(min_value=0)
-    name = serializers.CharField(max_length=50)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=256)
-    avatar = serializers.ImageField()
-    description = serializers.CharField(max_length=100)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password', ]
+
+    def create(self, validated_data):
+        return User(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id = validated_data.get('id', instance.id)
+        instance.rol = validated_data.get('rol', instance.rol)
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+        instance.description = validated_data.get('description', instance.description)
+        return instance
