58713fc484a290f43f2ff0420dd3a250e7f4b86a
Angel Pimentel <angelpimentelcontact@gmail.com>
2022-10-29 07:44:15 -0400

    [REMOVE] Logic of UserSerializer

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -5,17 +5,5 @@ from users.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ['password', ]
+        exclude = ('password', )
 
-    def create(self, validated_data):
-        return User(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.id = validated_data.get('id', instance.id)
-        instance.rol = validated_data.get('rol', instance.rol)
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.password = validated_data.get('password', instance.password)
-        instance.avatar = validated_data.get('avatar', instance.avatar)
-        instance.description = validated_data.get('description', instance.description)
-        return instance
